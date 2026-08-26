28874d2e83cbe346802fddacc95bfb9f546b955a
Calderon0723 <00215818@uca.edu.sv>
2026-08-26 16:54:27 -0600

    ADD: Prettier configuration :wrench:

---
+++ prettier.config.mjs
@@ -0,0 +1,7 @@
+/** @type {import("prettier").Config & import("prettier-plugin-tailwindcss").PluginOptions} */
+const prettierConfig = {
+  plugins: ["prettier-plugin-tailwindcss"],
+  tailwindStylesheet: "./src/app/globals.css",
+};
+
+export default prettierConfig;
