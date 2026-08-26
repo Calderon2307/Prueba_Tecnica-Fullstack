35e074c327ed242490ce229a0105c28d36ede3f6
Calderon0723 <00215818@uca.edu.sv>
2026-08-26 17:29:36 -0600

    FEAT: Add product domain interfaces :sparkles:

---
+++ src/lib/api/products.ts
@@ -0,0 +1,7 @@
+import {Product} from "@/types/product";
+
+const FAKE_STORE_API_URL = "https://fakestoreapi.com";
+
+export const getProducts = (): Promise<Product[]> => {};
+
+export const getProductById = (id: number): Promise<Product> => {}
