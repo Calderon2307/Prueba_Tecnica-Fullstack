2c053ac2e2b6f379c7776ccd6cb637a3d88ebd32
Calderon0723 <00215818@uca.edu.sv>
2026-08-27 15:29:17 -0600

    FEAT: Add Home page component :sparkles:

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,8 @@
-export default function Home() {
-  return <></>;
+import { getProducts } from "@/lib/api/products";
+import { ProductCatalog } from "@components/product/ProductCatalog";
+
+export default async function Home() {
+  const products = await getProducts();
+
+  return <ProductCatalog products={products} />;
 }
